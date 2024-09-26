1fcaa2b26c9196c7647cc185ec260265db43994c
Faizal <faisalrayeen517@gmail.com>
2024-09-26 13:01:59 +0530

    Update listdemo.py

--- listdemo.py
+++ listdemo.py
@@ -4,16 +4,61 @@
 
 
 # create list store AGE of your friends name -
-friendlist=["vansh","sakshi","vipul", "sahil"]
+#friendlist=["vansh","sakshi","vipul", "sahil"]
 
 # print the list of the friend
-print(friendlist)
+#print(friendlist)
 
 #the age of your friend name
 # append eill add all thedata into end of the list
 
-friendlist.append(36)
-friendlist.append(24)
-friendlist.append(43)
-friendlist.append(32)
-print(friendlist)
+#friendlist.append(36)
+#friendlist.append(24)
+#friendlist.append(43)
+#friendlist.append(32)
+#print(friendlist)
+
+# # add the data into list using index no-
+# friendlist.insert(3,"faizal")
+# print(friendlist)
+
+# #  to accs the data using index no-
+# # print(friendlist[3])
+
+# # access the  compllete list
+# for name in friendlist:
+#   print(name)
+
+
+# # to delete the name from the list-
+# # # remove will delete the data in the index;
+# # friendlist.remove("faizal")
+# # print(friendlist)
+
+
+# # pop will delete the data using index
+# friendlist.pop(1)
+# print(friendlist)
+
+# # access the complete list
+# # for name in friendlist:
+# # print(name)
+
+
+
+
+# wap to create the list of cities,and add one city in list and remove one city in index.with hwlp of pop func.
+citylist=["delhi","banglore","hydrabad","chandigarh"]
+print(citylist)
+
+citylist.insert(0,"kasol")
+print(citylist)
+
+citylist.remove("chandigarh")
+print(citylist)
+
+for name in citylist:
+  print(name)
+
+citylist.pop(1)
+print(citylist)
