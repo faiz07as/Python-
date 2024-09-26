097dd5f6d0cc29476987c6e537961363eebc8392
Faizal <faisalrayeen517@gmail.com>
2024-09-26 09:20:28 +0530

    Update and rename README.md to lec1-hell0 world.py

---
+++ lec1-hell0 world.py
@@ -0,0 +1,13 @@
+ # create a variable  to store the fees
+# fees="twenty thousand"
+# email="faisalrayeen@517gmail.com"
+# print(fees)
+# print(fees+" "+email)
+# print(f"my college fees is {fees} and email is {email}")
+
+# b=input("enter your first name:")
+# c=input("enter your last name:")
+# print(f"my full name  is{b} {c}")
+# b=input("enter the name:")
+# a=input("enter the email id:")
+# print(f"my full name is {b}  {a} ")
